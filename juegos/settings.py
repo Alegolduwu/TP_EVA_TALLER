1ada876b7ef4aa9159a6f09590aabe1f8ee9edc9
MicaelaLaime <laimemica@gmail.com>
2024-09-20 20:19:28 -0300

    ultimos cambios

--- juegos/settings.py
+++ juegos/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'videojuegos',
         'USER': 'root',   
-        'PASSWORD': '01102002Alegold', 
+        'PASSWORD': 'chestlusanibrika_1', 
         'HOST': 'localhost',    
         'PORT': '3306',        
     }
